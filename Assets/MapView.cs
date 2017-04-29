2891c71d4f4dd7ab3fb1cebbeed3c757569498f8
Jason Mitchell <foobardog@hotmail.com>
2017-04-29 05:29:45 -0700

    Rudimentary Map

--- Assets/MapView.cs
+++ Assets/MapView.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MapView : MonoBehaviour {
+public class MapView : MonoBehaviour
+{
+    public Scrollbar Scrollbar;
+    public RectTransform ViewportTransform;
+    public RectTransform ContentTransform;
+    public float ScrollbarDelta = 24.0f;
+    public Text[] MapNodes = new Text[20];
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        Scrollbar.value += Input.GetAxis("ScrollHistory") / ScrollbarDelta;
+
+        var rooms = GameManager.Instance.AllRooms;
+        for (int currentNode = 0; currentNode < MapNodes.Length; currentNode++)
+        {
+            MapNodes[currentNode].text = GameManager.Instance.GetRoomText(rooms[currentNode], false);
+        }
+    }
 }
